d377030217ee70a4049cb73ae79667edc7535d08
ElNirit <elnirit@gmail.com>
2021-08-01 11:14:07 +0300

    adding a json data file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import React from "react";
 import './App.css';
+import { Button, Card, Form } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from "./Page/header";
+import data from "./data/data.json"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       
-      </header>
+      <Header/>
     </div>
   );
 }
